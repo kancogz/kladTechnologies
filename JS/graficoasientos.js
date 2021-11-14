6b2d88368d6165c3b849eb3dc347d3c277fd2e09
KENNETH ANDRES CORRALES GONZALEZ <kcorrales@ucenfotec.ac.cr>
2021-11-14 11:27:27 -0600

    grafico asientos y seleccionarfuncion
    se juntan ambos html se hace un popup se validan datos.

--- JS/graficoasientos.js
+++ JS/graficoasientos.js
@@ -1,4 +1,61 @@
 'use strict'
 
-let cantidadAsientos
+const botonabrirseleccion=document.querySelector('#btnSelecAsientosFunc');
+
+const ContainerCine=document.querySelector('.GraficoSala');
+const asientos =document.querySelectorAll('.row .asientos:not(occupied)');
+
+const count =document.querySelector('#asientos #count');
+
+
+
+function updateSelectedAsientos(){
+    const selectedAsientos= document.querySelectorAll('.row .asientos.selected');
+
+   const asientosIndex = [...selectedAsientos].map(function(asiento){
+        return [...asientos].indexOf(asiento);
+    });
+    
+    localStorage.setItem('selectedAsientos', JSON.stringify(asientosIndex));
+
+
+
+    count.value=asientosIndex;
+
 }
+
+
+ContainerCine.addEventListener('click', (e) => {
+
+    if (e.target.classList.contains('asientos') && !e.target.classList.contains('occupied')) {
+       
+        e.target.classList.toggle('selected');
+        
+    }
+
+    updateSelectedAsientos();
+
+});
+
+
+
+const popup =()=>{
+
+    document.querySelector('.main-container').style.visibility='hidden';
+    document.querySelector('.main-container2').style.visibility='visible';
+
+
+
+}
+
+
+const popout =()=>{
+
+    document.querySelector('.main-container').style.visibility='visible';
+    document.querySelector('.main-container2').style.visibility='hidden';
+
+
+
+}
+
+
