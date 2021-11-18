7ca439d71aea2dc16015255ab0bca0c83ae608f3
enanipo <94089329+AndresAB2021@users.noreply.github.com>
2021-11-17 21:50:51 -0600

    Perfil Pelicula JavaScript
    La parte de calificación con estrellas ya debería ser funcional a nivel de interacción

---
+++ JS/Perfil_Pelicula.js
@@ -0,0 +1,22 @@
+const ratingStars = [...document.getElementsByClassName("rating__star")];
+
+function executeRating(stars) {
+    const starClassActive = "rating__star fas fa-star";
+    const starClassInactive = "ratins__star fa fa-star";
+    const starsLength = stars.length;
+    let i;
+
+    stars.map((star) => {
+        star.onclick = () => {
+           i = stars.indexOf(star);
+    
+           if (star.className===starClassInactive) {        
+              for (i; i >= 0; --i) stars[i].className = starClassActive;
+           } else {
+              for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
+           }
+        };
+    });
+}
+
+executeRating(ratingStars);
