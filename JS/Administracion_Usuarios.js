cbdc0cae31532feb2a8586136455fd18f46d9e54
enanipo <94089329+AndresAB2021@users.noreply.github.com>
2021-11-19 21:22:26 -0600

    Validación Administrar Usuarios
    Se agregó validación para administrar usuarios

---
+++ JS/Administracion_Usuarios.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const btnConfirmar = document.querySelector('#filter-btn')
+
+let inputsRequeridos = document.querySelectorAll('input:required');
+
+const validar = () => {
+    let error = false;
+    let inputsRequeridos = document.querySelectorAll('input:required');
+
+    inputsRequeridos.forEach(input=>{
+        if (input.value == '') {
+            error = true;
+            input.classList.add('error');
+        } 
+        else {
+            input.classList.remove('error');
+        }
+    });
+
+    /*codigo para sweet alert*/
+    if (error == true) {
+        Swal.fire({
+            'icon':'warning',
+            'title':'Ha Ocurrido un Error',
+            'text':'Por favor revise los campos resaltados'
+        });
+    
+        
+    }
+    /* termina swal */
+}
+
+btnConfirmar.addEventListener('click', validar );
