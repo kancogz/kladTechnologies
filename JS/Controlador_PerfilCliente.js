88adee517315e6a1dff3129431e92d363f375af8
enanipo <94089329+AndresAB2021@users.noreply.github.com>
2021-12-19 20:52:03 -0600

    Ver perfil
    Ver perfil cliente muestra datos de db reales

---
+++ JS/Controlador_PerfilCliente.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let usuarioActivo = sessionStorage.getItem('usuarioConectado');
+let usuario = {
+    correo : usuarioActivo
+}
+
+obtenerUsuario(usuario, '/obtener-usuario');
+
+if (sessionStorage.getItem('usuarioDatos')) {
+    let data = JSON.parse(sessionStorage.getItem('usuarioDatos'));
+    console.log(data.usuario.nombre);
+    document.getElementById('contenedorfoto').setAttribute('src', data.usuario.foto);
+    document.querySelector('#titulo1 h1').innerHTML = "Hola, " + data.usuario.nombre;
+    document.getElementById('name').innerHTML = data.usuario.nombre;
+    document.getElementById('apellido').innerHTML = data.usuario.apellidos;
+    document.getElementById('tipoCedula').innerHTML = data.usuario.tipoId;
+    document.getElementById('numeroCedula').innerHTML = data.usuario.numeroId;
+    document.getElementById('correo').innerHTML = data.usuario.correo;
+}else{
+    console.log("No puede ingresar");
+}
--- JS/Servicio-General.js
+++ JS/Servicio-General.js
@@ -11,6 +11,16 @@ const registrarDatos = async(pdatos, pendPoint) => {
     });
 };
 
+const obtenerUsuario = async(pdatos, pendPoint) => {
+    let url = `http://localhost:3000/api${pendPoint}`;
+
+    let peticion = await axios({
+        method : 'post',
+        url : url,
+        data : pdatos
+    });
+};
+
 const validarSesion = async(pdatos, pendPoint) => {
     let url = `http://localhost:3000/api${pendPoint}`;
     let respuesta = false;
@@ -25,13 +35,14 @@ const validarSesion = async(pdatos, pendPoint) => {
         respuesta = peticion.data.success;
         sessionStorage.setItem('conectado', peticion.data.success);
         sessionStorage.setItem('usuarioConectado', peticion.data.usuario.correo);
+        sessionStorage.setItem('usuarioDatos', JSON.stringify(peticion.data));
         sessionStorage.setItem('rolUsuario', peticion.data.usuario.rol);
     }else{
         respuesta = peticion.data.success;
     }
 
     return respuesta;
-}
+};
 
 const modificarDatos = async(pdatos, pendPoint, pUrlRedireccion) => {
 
